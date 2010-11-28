900fe09ace00d70c00a3d182999a7dc5c9f8ea3e
tar-to-key <coder3457@gmail.com>
2010-11-28 12:39:38 +0900

    generated by rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f9ea5f359df687fa19786d8c84c651378f943b9b24ae71e7fa47ea01f9836692cec32adc67390c2f661d6c77af5c360a7c8d629de740b0533fc237139a03f042'
